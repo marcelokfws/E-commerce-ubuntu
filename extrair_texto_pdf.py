003335384ae3db55d874bcc03229a4a75cab75d5
marcelokfws <30984850+marcelokfws@users.noreply.github.com>
2023-09-21 02:33:25 -0300

    Delete extrair_texto_pdf.py

--- extrair_texto_pdf.py
+++
@@ -1,7 +0,0 @@
-import PyPDF2
-
-pdfFileObj = open('Demonstrativo.pdf', 'rb')
-pdfReader  = PyPDF2.PdfFileReader(pdfFileObj)
-pageObj = pdfReader.getPage(0)
-text = pageObj.extract_text()
-print(text)
